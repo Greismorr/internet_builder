a7e7d7ade1aa5ea85d30e9c57e05a4929e3b75ad
greismorr <gabriel-reis1999@hotmail.com>
2022-07-13 11:09:14 -0300

    refactor: BlocListener and BlocBuilder to BlocConsumer

--- lib/presentation/page/internet_builder_widget.dart
+++ lib/presentation/page/internet_builder_widget.dart
@@ -25,9 +25,10 @@ class _InternetFailureWidgetState extends State<InternetBuilderWidget> {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: BlocListener(
+      child: BlocConsumer<InternetBuilderBloc, InternetBuilderState>(
         bloc: internetBuilderBloc..add(const GetConnectionStream()),
         listener: (context, state) {
+          // Prints an error if any
           if (state is CheckConnectionErrorState) {
             if (kDebugMode) {
               print(
@@ -36,29 +37,26 @@ class _InternetFailureWidgetState extends State<InternetBuilderWidget> {
             }
           }
         },
-        child: BlocBuilder<InternetBuilderBloc, InternetBuilderState>(
-          bloc: internetBuilderBloc,
-          builder: (context, state) {
-            if (state is ConnectionStreamRetrievedState) {
-              return StreamBuilder(
-                stream: state.connectionStream,
-                builder: (context,
-                    AsyncSnapshot<InternetConnectionStatus> snapshot) {
-                  if (snapshot.hasData) {
-                    if (snapshot.data ==
-                        InternetConnectionStatus.disconnected) {
-                      return widget.showWhenDisconnected;
-                    }
+        builder: (context, state) {
+          // Returns the StreamBuilder when the Connection Stream is acquired
+          if (state is ConnectionStreamRetrievedState) {
+            return StreamBuilder(
+              stream: state.connectionStream,
+              builder:
+                  (context, AsyncSnapshot<InternetConnectionStatus> snapshot) {
+                if (snapshot.hasData) {
+                  if (snapshot.data == InternetConnectionStatus.disconnected) {
+                    return widget.showWhenDisconnected;
                   }
+                }
 
-                  return widget.showWhenConnected;
-                },
-              );
-            }
+                return widget.showWhenConnected;
+              },
+            );
+          }
 
-            return widget.showWhenConnected;
-          },
-        ),
+          return widget.showWhenConnected;
+        },
       ),
     );
   }
